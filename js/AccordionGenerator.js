b5a17f4d10e02fb8011cbc741e76e92d5c6ba2c6
rayphelan <rayphelan@gmail.com>
2019-04-22 14:49:18 +0100

    Test HTML generation from AccordionGenerator.js

--- js/AccordionGenerator.js
+++ js/AccordionGenerator.js
@@ -2,20 +2,40 @@ class AccordionGenerator {
 
   constructor(targetElement, contents) {
 
+    // Class variables
+    this.targetElement = targetElement;
+    this.contents = contents;
+    this.html = '';
+
+    // Prepare HTML
+    this.prepare();
+
   }
 
   // Create section element
   section() {
-
+    return '<section></section>';
   }
 
   // Create article element
   article() {
-
+    return '<article></article>';
   }
 
   // Prepare html structure to load on page
   prepare() {
+
+    this.contents.forEach((option, index) => {
+
+      this.html += `
+      <div class="section-container">
+        ${this.section()}
+        ${this.article()}
+      </div>
+      `
+    });
+
+    console.log(`<div class="accordion">${this.html}</div>`)
 
   }
 
@@ -23,7 +43,7 @@ class AccordionGenerator {
   activate() {
     
   }
-  
+
 }
 
 export { AccordionGenerator }
