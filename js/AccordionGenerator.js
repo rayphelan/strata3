2c23c97c190cb37171a5403f2524ae31ccfe8e12
rayphelan <rayphelan@gmail.com>
2019-04-22 14:32:12 +0100

    Prepare AccordionGenerator.js class structure

--- js/AccordionGenerator.js
+++ js/AccordionGenerator.js
@@ -4,6 +4,26 @@ class AccordionGenerator {
 
   }
 
+  // Create section element
+  section() {
+
+  }
+
+  // Create article element
+  article() {
+
+  }
+
+  // Prepare html structure to load on page
+  prepare() {
+
+  }
+
+  // Activate Accordion
+  activate() {
+    
+  }
+  
 }
 
 export { AccordionGenerator }
